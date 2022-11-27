c8200d27a20b7eb58f0da2047f5879ce4f42602d
Daniil Taushkanov <118836201+Dalkory@users.noreply.github.com>
2022-11-27 22:23:16 +0300

    first version kod

--- kod.py
+++ kod.py
@@ -1,3 +1,10 @@
-a = [5,4,3,2,1]
-if a[3] == 2:
-    print(a)
+def calc_stat(listened):
+    return len(listened)
+    summ = 0
+    for i in listened:
+        summ += i
+    mins = summ//60
+    return mins
+print(
+    f'Вы прослушали {calc_stat([189, 148, 210, 144, 174, 158, 163, 189, 227, 198])}'
+    f'песен общей продолжительностью {mins}.')
